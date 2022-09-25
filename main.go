8698a5f3f056186183e9303da3539d529c052bfa
Umer Masood <umermasood.dev@gmail.com>
2022-09-25 14:54:58 +0500

    The root URL path doesn't act like a catch-all anymore

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, _ *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	_, _ = w.Write([]byte("Hello from snippet box"))
 }
 
-// Add a snippetView handler function
 func snippetView(w http.ResponseWriter, _ *http.Request) {
 	_, _ = w.Write([]byte("Display a specific snippet..."))
 }
 
-// Add a snippetCreate handler function
 func snippetCreate(w http.ResponseWriter, _ *http.Request) {
 	_, _ = w.Write([]byte("Create a new snippet..."))
 }
@@ -22,7 +24,6 @@ func snippetCreate(w http.ResponseWriter, _ *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	// registering two new handler functions and corresponding URL patterns
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	mux.HandleFunc("/snippet/view", snippetView)
 
