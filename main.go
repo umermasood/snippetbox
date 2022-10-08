af6e8be75557a44750d4b73ba32690cfdeb035a9
Umer Masood <umermasood.dev@gmail.com>
2022-10-08 19:49:38 +0500

    id query string param

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -13,8 +15,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte("Hello from snippet box"))
 }
 
-func snippetView(w http.ResponseWriter, _ *http.Request) {
-	_, _ = w.Write([]byte("Display a specific snippet..."))
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	_, _ = fmt.Fprintf(w, "Display a specific snippet by ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -27,30 +34,10 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte(`{"response":"Create a new snippet..."}`))
 }
 
-func manipulateHandler(w http.ResponseWriter, _ *http.Request) {
-	// Set a new cache-control header. If an existing "Cache-Control" header exists
-	// it will be overwritten.
-	w.Header().Set("cache-control", "public, max-age=31536000")
-	// In contrast, the Add() method appends a new "Cache-Control" header and can
-	// be called multiple times.
-	w.Header().Add("Cache-Control", "public")
-	w.Header().Add("Cache-Control", "max-age=31536000")
-	w.Header()["foo-header"] = []string{"bar-val1"}
-	// Delete all values for the "Cache-Control" header.
-	//w.Header().Del("Cache-Control")
-	// Retrieve the first value for the "Cache-Control" header.
-	log.Println(w.Header().Get("Cache-Control"))
-	// Retrieve a slice of all values for the "Cache-Control" header.
-	log.Println(w.Header().Values("Cache-Control"))
-
-	_, _ = w.Write([]byte("write to manipulate header"))
-}
-
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/snippet/create", snippetCreate)
 	http.HandleFunc("/snippet/view", snippetView)
-	http.HandleFunc("/snippet/manipulate/header", manipulateHandler)
 
 	log.Println("Starting the web server on port :4000")
 	log.Fatal(http.ListenAndServe(":4000", nil))
