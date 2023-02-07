16f27e3a16397d8fc3cbdbed66002000e771931b
M Umer Masood <umermasood.dev@gmail.com>
2023-02-07 11:27:44 +0500

    Updated snippetView handler to properly render snippets from db using view.tmpl

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -48,8 +48,29 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
+	// Initialize a slice containing the paths to the view.tmpl file,
+	// plus the base layout and navigation partial that we made earlier.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+	// Parse the template files...
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	// And then execute them. Notice how we are passing in the snippet
+	// data (a models.Snippet struct) as the final parameter?
+	err = ts.ExecuteTemplate(w, "base", snippet)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
 	// Write the snippet data as a plain-text HTTP response body.
-	_, _ = fmt.Fprintf(w, "%+v", snippet)
+	//_, _ = fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
